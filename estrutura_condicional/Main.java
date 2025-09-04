7b2c3e59d21d0ff18774db2715142c7daf849cd7
Douglas silva <douglaasbusiness@gmail.com>
2025-09-04 10:47:58 -0300

    Exercicios 1 de estrutura condicional

---
+++ estrutura_condicional/Main.java
@@ -0,0 +1,17 @@
+package estrutura_condicional;
+
+import java.util.Locale;
+import java.util.Scanner;
+
+public class Main {
+
+	public static void main(String[] args) {
+		Locale.setDefault(Locale.US);
+		String nome = "maria";
+		int Idade = 31;
+		double renda = 4000.0;
+		System.out.printf("A %s tem %d anos e recebe %.2f reais por mes", nome , Idade , renda );
+
+	}
+
+}
