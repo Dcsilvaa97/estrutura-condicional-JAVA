9dd127c5add49939ae15272a6e16794eaf7964b9
Douglas silva <douglaasbusiness@gmail.com>
2025-09-04 10:55:23 -0300

    Exercicios 5 de estrutura condicionais

---
+++ exercicios4_estrutura_condicional/Main.java
@@ -0,0 +1,25 @@
+package estrutura_condicional_exercicios4;
+
+import java.util.Locale;
+import java.util.Scanner;
+
+public class Main {
+
+	public static void main(String[] args) {
+		Locale.setDefault(Locale.US);
+		Scanner sc = new Scanner(System.in);
+		int horaInicial , horaFinal , duracao;
+		horaInicial = sc.nextInt();
+		horaFinal = sc.nextInt();
+		if(horaFinal > horaInicial) {
+			duracao = horaFinal - horaInicial;
+		}
+		else {
+			duracao = (24 - horaInicial) + horaFinal;
+		}
+		System.out.println("O jogo durou " + duracao + " horas");
+		sc.close();
+
+	}
+
+}
